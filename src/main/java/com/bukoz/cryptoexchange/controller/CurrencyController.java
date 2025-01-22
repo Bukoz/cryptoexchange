77e427f14eb6878253c428a2370f91a61ef1f422
Bukoz <xbukoz@gmail.com>
2025-01-22 18:19:49 +0100

    Split CurrencyController into two smaller classes

--- src/main/java/com/bukoz/cryptoexchange/controller/CurrencyController.java
+++ src/main/java/com/bukoz/cryptoexchange/controller/CurrencyController.java
@@ -1,10 +1,7 @@
 package com.bukoz.cryptoexchange.controller;
 
 import com.bukoz.cryptoexchange.model.CurrencyRate;
-import com.bukoz.cryptoexchange.model.ExchangeRequest;
-import com.bukoz.cryptoexchange.model.ExchangeResponse;
 import com.bukoz.cryptoexchange.service.CurrencyService;
-import com.bukoz.cryptoexchange.service.ExchangeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +16,9 @@ import java.util.List;
 public class CurrencyController {
 
     private final CurrencyService currencyService;
-    private final ExchangeService exchangeService;
 
-    public CurrencyController(CurrencyService currencyService, ExchangeService exchangeService) {
+    public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
-        this.exchangeService = exchangeService;
     }
 
     @GetMapping("/{currency}")
@@ -32,13 +27,6 @@ public class CurrencyController {
             @RequestParam(value = "filter[]", required = false) List<String> filters) throws IOException, URISyntaxException {
         log.info("Currency rate GET call for {} with filter {}", currency, filters);
         CurrencyRate response = currencyService.getCurrencyRates(currency, filters);
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/exchange")
-    public ResponseEntity<ExchangeResponse> getExchange(@RequestBody ExchangeRequest request) throws IOException, URISyntaxException {
-        log.info("Currency exchange POST call");
-        ExchangeResponse response = exchangeService.getExchange(request);
         return ResponseEntity.ok(response);
     }
 
---
+++ src/main/java/com/bukoz/cryptoexchange/controller/ExchangeController.java
@@ -0,0 +1,33 @@
+package com.bukoz.cryptoexchange.controller;
+
+import com.bukoz.cryptoexchange.model.ExchangeRequest;
+import com.bukoz.cryptoexchange.model.ExchangeResponse;
+import com.bukoz.cryptoexchange.service.ExchangeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@Slf4j
+@RestController
+@RequestMapping("/currencies")
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
+    @PostMapping("/exchange")
+    public ResponseEntity<ExchangeResponse> getExchange(@RequestBody ExchangeRequest request) throws IOException, URISyntaxException {
+        log.info("Currency exchange POST call");
+        ExchangeResponse response = exchangeService.getExchange(request);
+        return ResponseEntity.ok(response);
+    }
+}
